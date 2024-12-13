6a54a856ee62699417c57f417c80000a7de34514
vennela2523 <vennelakothoju@gmail.com>
2024-12-13 23:40:44 +0530

    files created

---
+++ index.js
@@ -0,0 +1,34 @@
+ var userinput=prompt("enter your age");
+ if(userinput>=18){
+    console.log("you are eligible to vote");
+    
+ }else{
+    console.log("you are not eligible to vote");
+    
+ }
+ var userinput1=prompt("enter your age for driving licence");
+ if(userinput1>=18){
+    console.log("you are eligible to get driving licence");
+    
+ }else{
+    console.log("you are not eligible to driving licence");
+    
+ }
+ var userinput2=prompt("enter your age");
+ 
+ if(userinput1>=21){
+    console.log("you are eligible marriage");
+    
+ }else{
+    console.log("you are not eligible for marriage");
+    
+ }
+ var userinput3=prompt("enter your marks percentage");
+ 
+ if(userinput1>=75){
+    console.log("you are pass in exams");
+    
+ }else{
+    console.log("you are detained in exams");
+    
+ }
